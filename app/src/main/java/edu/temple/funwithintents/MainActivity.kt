85f41bf4273f1faf0af268ee19b26bee8ed0ca02
CheK_code <tup90909@temple.edu>
2025-11-06 15:11:36 -0500

    created intent object and statement for sharing text data to contacts via the messaging app.

--- app/src/main/java/edu/temple/funwithintents/MainActivity.kt
+++ app/src/main/java/edu/temple/funwithintents/MainActivity.kt
@@ -1,21 +1,37 @@
 package edu.temple.funwithintents
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("UnsafeIntentLaunch")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // This view contains the text to share
         val editText = findViewById<EditText>(R.id.editTextText)
+        val shareButton = findViewById< ImageButton>(R.id.shareImageButton)
 
-        // When the user clicks this button, share the text if not empty
-        findViewById<ImageButton>(R.id.shareImageButton).setOnClickListener {
+        shareButton.setOnClickListener {
+            val textToShare = editText.text.toString().trim()
 
+            if (textToShare.isNotEmpty()) {
+                val shareIntent = Intent().apply {
+                    action = Intent.ACTION_SEND
+                    putExtra(Intent.EXTRA_TEXT, textToShare)
+                    type = "text/plain"
+                }
+
+                startActivity(Intent.createChooser(shareIntent, "Share Via"))
+            } else {
+                Toast.makeText(this, "Enter text to share", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
